ebb3cda832be2ebc1a1f751d34a4b5eb5c8004f4
ShahdAbdulhamid <shahd.w1205@gmail.com>
2026-05-16 16:30:43 +0300

    edited in program

--- main.c
+++ main.c
@@ -5,7 +5,21 @@
 
 int main() {
     parseFile("program.txt");
-    while (pc < NumberofInstructions)
-        fetch();
+    int cycle = 1;
+    while (pc < NumberofInstructions) {
+    printf("Clock Cycle %d\n", cycle++);
+    fetch();
+    }
+    printf("\nFinal Registers:\n");
+    for (int i = 0; i < 64; i++)
+        printf("R%d = %d\n", i, registerFile[i]);
+    printf("PC = %u\n", pc);
+    printf("SREG = %u\n", sreg);
+
+    printf("\nData Memory:\n");
+    for (int i = 0; i < 2048; i++)
+        if (dataMemory[i] != 0)
+            printf("dataMemory[%d] = %d\n", i, dataMemory[i]);
+
     return 0;
 }
